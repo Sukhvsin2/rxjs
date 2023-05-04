4468aa4165a0b7dd5dc4d5cf228686c3e46ea349
Sukhvinder Singh <sukhvindersingh@Sukhvinders-MacBook-Air.local>
2023-05-04 14:42:14 -0400

    strict rule changed to false

--- src/app/state/state.component.ts
+++ src/app/state/state.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-state',
@@ -6,6 +7,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./state.component.scss']
 })
 export class StateComponent implements OnInit {
+
+  agents: Observable<string>;
 
   constructor() { }
 
