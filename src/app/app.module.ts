9655973a9d217429ff30da58ccd29c1c7aa90c3a
Sukhvinder Singh <sukhvindersingh@Sukhvinders-MacBook-Air.local>
2023-05-04 14:33:38 -0400

    state module init

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { StateComponent } from './state/state.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    StateComponent
   ],
   imports: [
     BrowserModule,
